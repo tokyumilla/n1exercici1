26b8e20900b7f0f53f1f91fa58507b92862eed62
tokyumilla <tokyumilla@gmail.com>
2022-10-27 16:40:26 +0200

    Corrección, bloque sout sin inicializar variables

--- src/n1exercici1/Instrument.java
+++ src/n1exercici1/Instrument.java
@@ -1,18 +1,23 @@
 package n1exercici1;
 
 public abstract class Instrument {
-    private static String name;
-    private static int price;
+    private String name;
+    private int price;
 
     public abstract void tocar ();
 
-    static {
+    {
         name = "instrumento por defecto";
         price = 0;
-        System.out.println("Se han asignado valores por defecto");
+        System.out.println("Se han asignado valores por defecto. Este mensaje se reproducirá cada vez que se instancie un objeto");
+    }
+
+    static {
+        System.out.println("Este mensaje sólo aparecerá una vez");
     }
     //Al realizar un bloque estático sólo se ejecutará una vez, si no fuera estático este código se
     //ejecutaría cada vez que se instancie un objeto.
+
 
 
     public Instrument () {}
